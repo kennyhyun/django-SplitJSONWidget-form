0d6fc2ce1351972a018c0274f0d6f27eddfc7f59
Kyunghyun Yeo <kenny@yeoyou.net>
2014-09-21 08:28:27 +0900

    split up models into admin

--- example/testjson/admin.py
+++ example/testjson/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django import forms
+
+from models import JsonTest
+from splitjson.widgets import SplitJSONWidget
+
+
+class JsonTestAdminForm(forms.ModelForm):
+    test = forms.CharField(widget=SplitJSONWidget(attrs={'class': 'json-response-type', 'size': '40'},
+        debug=True),
+        initial={'data1': ''})
+    test2 = forms.CharField(widget=SplitJSONWidget(attrs={'class': 'json-response-type', 'size': '40'}, 
+        debug=True),
+        initial={'data2': ''})
+
+class JsonTestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'test',)
+    form = JsonTestAdminForm
+
+admin.site.register(JsonTest, JsonTestAdmin)
